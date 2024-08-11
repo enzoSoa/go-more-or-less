49fd781fe2b242529669f79589f417c152539427
enzoSoa <enzoayrsoares@gmail.com>
2024-08-11 17:19:05 +0200

    Segmanting in functions

--- src/main.go
+++ src/main.go
@@ -2,25 +2,37 @@ package main
 
 import "fmt"
 
+func getUserGuess() int {
+  var guess int
+  fmt.Scanf("%v", &guess)
+  
+  return guess
+}
+
+func compareGuess(number, guess int) bool {
+  switch {
+    case guess < number:
+      fmt.Println("More")
+    case guess > number:
+      fmt.Println("Less")
+    default:
+      return true
+  }
+  return false
+} 
+
 func main() {
   const NUMBER_TO_GUESS int = 23
   fmt.Println("Enter a number between 0 and 100:")
 
   gameOver := false
   var guesses []int
-  for !gameOver {
-    var guess int
-    fmt.Scanf("%v", &guess)
 
+  for !gameOver {
+    guess := getUserGuess()
+    gameOver = compareGuess(NUMBER_TO_GUESS, guess)
     guesses = append(guesses, guess)
-    switch {
-    case guess < NUMBER_TO_GUESS:
-      fmt.Println("More")
-    case guess > NUMBER_TO_GUESS:
-      fmt.Println("Less")
-    default:
-      gameOver = true
-      fmt.Println("GG!!", "You won in", len(guesses), "guesses")
-    }
   }
+
+  fmt.Println("GG!!", "You won in", len(guesses), "guesses")
 }
